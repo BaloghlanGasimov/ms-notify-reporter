32b8d850791f931bbcc7aefb84b040e697d5157e
Baloghlan <qasimovbaloglan13@gmail.com>
2024-07-21 15:02:46 +0400

    spring initializing

---
+++ src/main/java/com/example/msnotifyreporter/MsNotifyReporterApplication.java
@@ -0,0 +1,13 @@
+package com.example.msnotifyreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotifyReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotifyReporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msnotifyreporter/MsNotifyReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msnotifyreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotifyReporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
